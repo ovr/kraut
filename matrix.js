1fa3becbecd4ba403da5b72fd0148c580d8ef562
Дмитрий Пацура <talk@dmtry.me>
2014-11-30 14:47:04 +0900

    Split

---
+++ matrix.js
@@ -0,0 +1,124 @@
+/**
+ *
+ * @param matrix
+ * @param n
+ * @returns {*}
+ */
+function matrixDet(matrix, n) {
+    var i, j;
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            if (matrix[i][j] == 0) {
+                /**
+                 * Матрицу хоть с 1 нулевым числом нельзя использовать для данного алгоритма
+                 */
+                return false;
+            }
+        }
+    }
+
+    var scaling = new Array(n);
+
+    var tmp;
+
+    for (i = 0; i < n; i++) {
+        tmp = 0;
+
+        for (j = 0; j < n; j++) {
+            if (Math.abs(matrix[i][j]) > tmp) {
+                tmp = Math.abs(matrix[i][j]);
+            }
+        }
+
+        /**
+         * @todo Проверить долбаеб ли я?
+         * @type {number}
+         */
+        scaling[i] = (1 / tmp) / 1;
+//                console.log(tmpMatrix[i]);
+    }
+
+    var sign = 1;
+    var summ;
+
+    for (j = 0; j < n; j++) {
+        for (i = 0; i < j; i++) {
+            summ = matrix[i][j];
+
+            for (k = 0; k < i; k++) {
+                console.log("Worflow SUMM matrix[" + i + "][" + k + "] = " + matrix[i][k]);
+                summ -= matrix[i][k] * matrix[k][j];
+            }
+
+            console.log("1 Workflow j = " + j + " i = " + i + " summ = " + summ);
+            matrix[i][j] = summ;
+        }
+        console.log("Matrix round A " + matrix);
+
+        tmp = 0;
+        var imax = -1;
+
+        for (i = j; i < n; i++) {
+            summ = matrix[i][j];
+
+            for (k = 0; k < j; k++) {
+                summ -= matrix[i][k] * matrix[k][j];
+            }
+
+            console.log("Summ matrix[" + i + "][" + j + "] = " + summ);
+            matrix[i][j] = summ;
+            var cur = Math.abs(summ);
+
+            if (cur.compareTo(tmp) >= 0) {
+                tmp = cur;
+                imax = i;
+            }
+        }
+        console.log("Matrix round B " + matrix);
+
+        console.log("Imax = " + imax);
+        if (j != imax) {
+            var t;
+
+            for (k = 0; k < n; k++) {
+                t = matrix[j][k];
+                matrix[j][k] = matrix[imax][k];
+                matrix[imax][k] = t;
+            }
+
+            t = scaling[imax];
+            scaling[imax] = scaling[j];
+            scaling[j] = t;
+
+            sign = -sign;
+        }
+
+        console.log("Matrix round " + matrix);
+        if (j != n - 1) {
+            for (i = j + 1; i < n; i++) {
+                console.log("DIVIDE " + matrix[i][j] + " [" + i + "][" + j + "].devide(" + matrix[j][j] + ", 100, 6) = " + matrix[i][j].divide(matrix[j][j], 100, 6));
+                matrix[i][j] = matrix[i][j].divide(matrix[j][j], 100, 6);
+            }
+        }
+
+        console.log("\n\n");
+    }
+
+    console.log("\n\n");
+    console.log("Sign = " + sign);
+
+    var result = 1;
+    if (sign == -1) {
+        result = -1;
+    }
+
+    console.log("Result before multiple " + result);
+    for (i = 0; i < n; i++) {
+        console.log("Multiple i = " + i + " value = " + matrix[i][i]);
+        result *= matrix[i][i];
+    }
+
+    console.log("Result before devide " + result);
+    return result.divide(1, 0, 6);
+}
